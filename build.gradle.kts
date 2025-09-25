ceb5abcfccb21ab7676c05fc9287dab139477016
onoteruaki <terusan.o@icloud.com>
2025-09-25 18:21:59 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+    id("com.thinkimi.gradle.MybatisGenerator")version "2.4"
+
+}
+
+group = "com.book.manager"
+version = "0.0.1-SNAPSHOT"
+description = "Book Manager project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.5")
+
+    implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.5.2")
+	runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.2")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+mybatisGenerator {
+	verbose = true
+	configFile = file("${projectDir}/src/main/resources/generatorConfig.xml")
+}
---
+++ src/main/kotlin/com/book/manager/book_manager/domain/RoleType.kt
@@ -0,0 +1,3 @@
+package com.book.manager.book_manager.domain
+
+enum class RoleType { ADMIN, User }
